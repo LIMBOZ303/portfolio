6f7eb78b218bd5fffe5469d7c887bf7dd007f50d
Illusion1 <nguyencuong.20082009@gmail.com>
2026-05-15 06:01:44 +0700

    fix: move base path configuration to root of vite config object

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [
     react(),
     tailwindcss(),
-    base: '/portfolio/',
   ],
 })
